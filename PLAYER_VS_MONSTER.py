0d4d9f8a359ff7e3688c51252e72ccb9c526c2a3
Mirfozil Mirazamov <57904040+mirfozil99@users.noreply.github.com>
2025-12-10 18:24:39 +0500

    Add files via upload

---
+++ PLAYER_VS_MONSTER.py
@@ -0,0 +1,149 @@
+import random
+import time
+playerheal=100
+monsterheal=100
+MaxHP=100
+MaxHM=100
+healP=10
+healM=15
+damageP=10
+damageM=25
+mw=0
+pw=0
+NUM=1
+print(f"""
+____________________________________________________________________________________________
+Player N{NUM}                                            Monster
+health {MaxHP}/{playerheal}                              health {MaxHM}/{monsterheal}
+ 
+       *                                           ***
+      /|->                                       <<|||
+       ||                                          ???
+    -------------------------------------------------------------
+1) attack (damge P=15, M=25)
+2) heal (heals P=10 ,M=15)
+3) skip (skips the turn)
+______________________________________________________________________________________________
+""")
+while True:
+    while True:
+            try:
+                turn=input("choose your movement : ")
+                if len(turn)==1:
+                    turn=int(turn)   
+                    break
+                else:
+                  print("""
+Wrong input, give 1,2,3 based on below options:
+1) attack (damge P=15, M=25)
+2) heal (heals P=10 ,M=15)
+3) skip (skips the turn)
+""")
+            except:
+                 print("""
+Wrong input, give 1,2,3 based on below options:
+1) attack (damge P=15, M=25)
+2) heal (heals P=10 ,M=15)
+3) skip (skips the turn)
+""")
+
+    if turn == 1:
+        monsterheal-=damageP
+        print(f"""Players pick -- attack
+                given damage({damageP})
+             
+             
+             """)
+    elif turn == 2:
+        if playerheal !=MaxHP:
+            playerheal+=healP
+        print(f"""Players pick -- heal
+                given heal({healP})
+             
+             
+             """)
+    elif turn == 3:
+       print("""players pick -- skip
+             
+             
+             
+             """)
+
+    print(f"""
+____________________________________________________________________________________________
+Player N1                                            Monster
+health {MaxHP}/{playerheal}                              health {MaxHM}/{monsterheal}
+    
+        *                                           ***
+        /|->                                       <<|||
+        ||                                          ???
+    -------------------------------------------------------------
+1) attack (damge P=15, M=25)
+2) heal (heals P=10 ,M=15)
+3) skip (skips the turn)
+______________________________________________________________________________________________
+    """)
+    if monsterheal==0:
+        pw=1
+        break
+    time.sleep(1)
+    print("monster picking")
+    time.sleep(0.5)
+    print(".")
+    time.sleep(0.5)
+    print(".")
+    time.sleep(0.5)
+    print(".")
+    monstarpick=random.randint(1,3)
+    if monstarpick == 1:
+        playerheal-=damageM
+        print(f"""Monsters pick -- attack
+                given damage({damageM})
+             
+             
+             """)
+    elif monstarpick == 2:
+        if monsterheal !=MaxHM:
+            monsterheal+=healM
+        print(f"""Monsters pick -- heal
+                given heal({healM})
+             
+             
+             """)
+    elif monstarpick == 3:
+       print("""Monsters pick -- skip
+             
+             
+             
+             """)
+    print(f"""
+____________________________________________________________________________________________
+Player N1                                           Monster
+health {MaxHP}/{playerheal}                         health {MaxHM}/{monsterheal}
+    
+        *                                           ***
+        /|->                                       <<|||
+        ||                                          ???
+    -------------------------------------------------------------
+1) attack (damge P=15, M=25)
+2) heal (heals P=10 ,M=15)
+3) skip (skips the turn)
+____________________________________________________________________________________________
+    """)
+    if playerheal==0:
+        mw=1
+        break
+if mw==1:
+    print("""
+************************************************************
+          winer winer chiken diner 
+                 Monter Winer
+************************************************************
+""")
+elif pw==1:
+    print(f"""
+************************************************************
+          winer winer chiken diner 
+                 Player{NUM} Winer
+************************************************************
+""")
